ce8eca94a0cce3220413e3f44f62592092f140d0
Wei-Jie Huang <weii41392@gmail.com>
2024-03-04 04:16:05 +0000

    autotune

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -6,21 +6,21 @@ init_r = 0; -- in degrees
 
 -- Initialize()
 M_PI = 3.14159265358979323846264338327950288;
-x_std = 0.0; -- [0, 1] with step = 0.1
-y_std = 0.0; -- [0, 1] with step = 0.1
-r_std = 0.0; -- [0, M_PI/2] with step = M_PI / 18 (10 degrees)
+x_std = 0.5;
+y_std = 0.5;
+r_std = 0.0;
 
 -- Predict()
 -- trans error from trans model
-k1 = 0.1; -- [0.1, 1] with step = 0.1
+k1 = 0.5;
 -- trans error from rotat model
-k2 = 0.1; -- [0.01, 0.1] with step = 0.01
+k2 = 0.1;
 -- rotat error from trans model
-k3 = 0.1; -- [0.01, 0.1] with step = 0.01
+k3 = 0.1;
 -- rotat error from rotat model
-k4 = 0.1; -- [0.1, 1] with step = 0.1
+k4 = 0.5;
 
 --Update()
-sigma_s = 1.0; -- [1.0, 3.0] with step = 0.5
-gamma_pow = -1; -- [-1.0, 0.0] with step = 0.1
-d_short_d_long = 1.0; -- (0.5, 1, 3, 5)
+sigma_s = 2;
+gamma_pow = -1;
+d_short_d_long = 0.5;
