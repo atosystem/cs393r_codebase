fc6afc32f2a9b407a201e0bb3f148894ff9241e5
atosystem <yjshih23@gmail.com>
2024-04-26 14:13:11 -0500

    remove unused params

--- config/slam.lua
+++ config/slam.lua
@@ -4,10 +4,11 @@ min_trans_diff_between_nodes = 1;
 min_angle_diff_between_nodes = 3.14 / 6.0;
 
 -- Motion Model --------------------------------
-motion_model_trans_err_from_trans = 0.4;
-motion_model_trans_err_from_rot = 0.4;
-motion_model_rot_err_from_trans = 0.4;
-motion_model_rot_err_from_rot = 0.4;
+-- this part is moved to slam.cc
+-- motion_model_trans_err_from_trans = 0.4;
+-- motion_model_trans_err_from_rot = 0.4;
+-- motion_model_rot_err_from_trans = 0.4;
+-- motion_model_rot_err_from_rot = 0.4;
 
 
 -- PoseGraph Parameters --------------------------------
