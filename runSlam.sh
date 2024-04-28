5f9c301db07d99878faa3e21e39e3fad2ec5ba02
atosystem <yjshih23@gmail.com>
2024-04-28 15:35:43 -0500

    revise runSlam

--- runSlam.sh
+++ runSlam.sh
@@ -1,14 +1,16 @@
-link_file="config/realCarExp/current_slam"
+link_file="/home/dev/cs393r_codebase/config/realCarExp/current_slam.lua"
 
 if [ -L "$link_file" ]; then
     # echo "Symbolic link file $link_file exists. Deleting..."
     rm "$link_file"
     # echo "Symbolic link file deleted."
 else
-    # echo "Symbolic link file $link_file does not exist."
+    echo "Symbolic link file $link_file does not exist."
 fi
-echo "Createing link .lua: $1"
-ls -s ./config/realCarExp/$1.lua ./config/realCarExp/current_slam.lua
+echo "Creating link .lua: $1"
+ln -s /home/dev/cs393r_codebase/config/realCarExp/$1.lua   /home/dev/cs393r_codebase/config/realCarExp/current_slam.lua
 
-wcho "Start slam"
+#ln -s /home/dev/cs393r_codebase/config/realCarExp/offline.lua /home/dev/cs393r_codebase/config/realCarExp/current_slam.lua
+
+echo "Start slam"
 ./bin/slam
